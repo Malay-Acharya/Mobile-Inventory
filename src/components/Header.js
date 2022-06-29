eb166d345eb266a3456b124138ed8cd0c98627f4
Malay <malayacharya21@gmail.com>
2022-06-29 15:31:26 +0530

    Made the header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='h-40 bg-black text-white flex-col pl-14 pt-6'>
+        <h1 className='text-3xl'>Mobile Store</h1>
+        <h3 className='text-lg'>
+            Random lorem ipsum text
+        </h3>
+    </div>
+  )
+}
