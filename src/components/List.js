1290ad16a342e231ca36884a55ecd20f4331ba93
Malay <malayacharya21@gmail.com>
2022-06-30 16:21:11 +0530

    delete item works with list view

--- src/components/List.js
+++ src/components/List.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+import { DELETEITEM } from '../actions';
+import { useDispatch } from 'react-redux/es/hooks/useDispatch';
 
 export default function MainList() {
+  const dispatch = useDispatch();
   const list = useSelector(state => state.changeinventory);
   return (
     <div className='my-4 flex flex-col gap-4 ml-2'>
       {list.map(item => {
         return(
-          <div key = {item.id} className=' pl-4 py-1 border-2 border-black cursor-pointer flex gap-4'>
-            <h2>{item.name}</h2>
+          <div key = {item.id} className=' pl-4 py-1 border-2 border-black flex gap-4'>
+            <h2 className='cursor-pointer'>{item.name}</h2>
             <h2>Price: {item.price}</h2>
             <h2>Available Units: {item.units}</h2>
+            <h2 onClick={() => {dispatch(DELETEITEM(item))}} className = 'absolute right-32 cursor-pointer'>Delete</h2>
           </div>
         );
       })}
